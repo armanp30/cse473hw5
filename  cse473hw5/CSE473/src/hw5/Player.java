3c5007ff6d4d70972fc04d37b7e920137135c6f1
alex.l.meng <alex.l.meng@6310692e-6f4c-0410-8e40-b5de99086b13>
2008-05-04 22:41:00 +0000

    Added comments to Player interface.

---  cse473hw5/CSE473/src/hw5/Player.java
+++  cse473hw5/CSE473/src/hw5/Player.java
@@ -4,6 +4,10 @@ package hw5;
  * CSE 473 Spring 2007
  * Homework 4, Tic Tac Toe, Minimax & Alpha Pruning
  */
+/**
+ * This is an interface used to define the expected behaviors
+ * for players.
+ */
 public interface Player {
 	public void makeMove(Board state);
 	public String getMarker();
