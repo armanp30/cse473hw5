c2ba9554035d8a01cceeca57741970d4d82f5672
alex.l.meng <alex.l.meng@6310692e-6f4c-0410-8e40-b5de99086b13>
2008-05-04 23:09:34 +0000

    Added comments to the move.java

--- trunk/ cse473hw5/CSE473/src/hw5/Move.java
+++ trunk/ cse473hw5/CSE473/src/hw5/Move.java
@@ -2,12 +2,32 @@ package hw5;
 /**
  * Alex Meng
  * CSE 473 Spring 2007
- * Homework 4, Tic Tac Toe, Minimax & Alpha Pruning
+ * Homework 5, Othello, Move.java
+ */
+
+/**
+ * This class is designed to encapsulate the following information for a Move object.
+ * 
+ * Coordinates on the board (x,y), the marker that occupies it, null if no marker occupies it
+ * and the value of the move.
  */
 public class Move {
+	//fields used to represent the x and y coordinate.
 	public int x,y;
+	
+	//the value associated with the move.
 	public int value;
+	
+	//field indicating what marks the move.
 	public String marker;
+	/**
+	 * Constructs a move object based on the coordinates, marker, and value passed.
+	 * 
+	 * @param x an int, the x coordinate of the move.
+	 * @param y an int, the y coordinate of the move.
+	 * @param marker a String object, representing the marker to occupy the move.
+	 * @param value an int, representing the value of the move.
+	 */
 	public Move(int x, int y, String marker, int value) {
 		this.x = x;
 		this.y = y;
