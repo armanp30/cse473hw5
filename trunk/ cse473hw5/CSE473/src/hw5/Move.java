87fa69663ecb46535f1d2196c7dcec6d459422fd
alex.l.meng <alex.l.meng@6310692e-6f4c-0410-8e40-b5de99086b13>
2008-05-04 22:36:47 +0000

    Initial import.

---
+++ trunk/ cse473hw5/CSE473/src/hw5/Move.java
@@ -0,0 +1,20 @@
+package hw5;
+/**
+ * Alex Meng
+ * CSE 473 Spring 2007
+ * Homework 4, Tic Tac Toe, Minimax & Alpha Pruning
+ */
+public class Move {
+	public int x,y;
+	public int value;
+	public String marker;
+	public Move(int x, int y, String marker, int value) {
+		this.x = x;
+		this.y = y;
+		this.marker = marker;
+		this.value = value;
+	}
+	
+	
+	
+}
---
+++ trunk/ cse473hw5/CSE473/src/hw5/Othello.java
@@ -0,0 +1,47 @@
+package hw5;
+/**
+ * Alex Meng
+ * CSE 473 Spring 2007
+ * Homework 5, Othello Driver class
+ */
+import java.util.ArrayList;
+
+public class Othello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	
+		ArrayList<Player> players = new ArrayList<Player>();
+		
+		players.add( new HumanPlayer("B"));
+		players.add( new HumanPlayer("W"));
+		//players.add(new AIPlayer("W",false));
+		
+		Board gameBoard = new Board(8);
+		
+		int turn = 0;
+		System.out.println("***********************");
+		System.out.println("Let's Play Othello!");
+		System.out.println("***********************");
+		
+		while ( !gameBoard.isGameOver() ) {
+			players.get(turn).makeMove(gameBoard);
+			gameBoard.printBoard();
+			turn++;
+			turn = turn % players.size();
+		}
+		
+		System.out.println("********************");
+		System.out.println("Game over!");
+		
+		String winner = gameBoard.getWinner();
+		if ( winner!=null) 			
+			System.out.println(winner + " is the winner!");
+		else
+			System.out.println("The game is a draw!");
+
+	}
+
+}
