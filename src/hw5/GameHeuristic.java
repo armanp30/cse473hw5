6560e11a32f4fbeeffd97f86db0df259a6787488
alex.l.meng <alex.l.meng@6310692e-6f4c-0410-8e40-b5de99086b13>
2008-05-06 07:19:37 +0000

    Added code to allow for undoing moves

--- src/hw5/GameHeuristic.java
+++ src/hw5/GameHeuristic.java
@@ -17,19 +17,20 @@ public class GameHeuristic {
 	
 	private static Move maxValue(Board board) {
 		Move result = new Move(0,0,null,-2);
+		ArrayList<Move> conversions;
 		
 		ArrayList<Move> childrens = new ArrayList<Move>();//board.getEmptySpots();
 		for (int i=0; i<childrens.size(); i++ ) {
 			Move temp = childrens.get(i);
 			temp.marker = "X";
-			board.makeMove(temp);
+			conversions = board.makeMove(temp);
 			if  ( terminalTest(board) )
 				temp.value = utility(board);
 			else
 				temp.value = minValue(board).value;
 			if (result.value < temp.value)
 				result = temp;
-			board.undoMove(temp.x, temp.y);
+			board.undoMove(temp, conversions);
 		}
 	
 		return result;
@@ -37,18 +38,20 @@ public class GameHeuristic {
 	
 	private static Move minValue(Board board) {
 		Move result = new Move(0,0,null,2);
+		ArrayList<Move> conversions;
+		
 		ArrayList<Move> childrens = board.getLegalMoves(null); //getEmptySpots();
 		for (int i=0; i<childrens.size(); i++ ) {
 			Move temp = childrens.get(i);
 			temp.marker = "O";
-			board.makeMove(temp);
+			conversions = board.makeMove(temp);
 			if  ( terminalTest(board) )
 				temp.value = utility(board);
 			else
 				temp.value = maxValue(board).value;
 			if (result.value >= temp.value)
 				result = temp;
-			board.undoMove(temp.x, temp.y);
+			board.undoMove(temp, conversions);
 		}
 		return result;
 	}
@@ -79,18 +82,20 @@ public class GameHeuristic {
 	
 	public static Move maxValue(Board board, int alpha, int beta) {
 		Move result = new Move(0,0,null,-2);
+		ArrayList<Move> conversions;
+		
 		ArrayList<Move> childrens = board.getLegalMoves(null); //getEmptySpots();
 		for (int i=0; i<childrens.size(); i++ ) {
 			Move temp = childrens.get(i);
 			temp.marker = "X";
-			board.makeMove(temp);
+			conversions = board.makeMove(temp);
 			if  ( terminalTest(board) )
 				temp.value = utility(board);
 			else
 				temp.value = minValue(board).value;
 			if (result.value < temp.value)
 				result = temp;
-			board.undoMove(temp.x, temp.y);
+			board.undoMove(temp, conversions);
 			if (result.value >= beta)
 				return result;
 			alpha = Math.max(alpha, result.value);
@@ -101,18 +106,20 @@ public class GameHeuristic {
 	
 	public static Move minValue(Board board, int alpha, int beta) {
 		Move result = new Move(0,0,null,2);
+		ArrayList<Move> conversions;
+		
 		ArrayList<Move> childrens = board.getLegalMoves(null);//();
 		for (int i=0; i<childrens.size(); i++ ) {
 			Move temp = childrens.get(i);
 			temp.marker = "O";
-			board.makeMove(temp);
+			conversions = board.makeMove(temp);
 			if  ( terminalTest(board) )
 				temp.value = utility(board);
 			else
 				temp.value = maxValue(board).value;
 			if (result.value >= temp.value)
 				result = temp;
-			board.undoMove(temp.x, temp.y);
+			board.undoMove(temp, conversions);
 			if (result.value <= alpha)
 				return result;
 			beta = Math.min( beta, result.value);
